8e2413bf1041a3f8728c63c3b6e686e6b27ca62a
Fiora Laurent <groudanex@gmail.com>
2022-01-21 21:41:02 -0500

    added turn random

--- assets/js/game.js
+++ assets/js/game.js
@@ -31,9 +31,14 @@ var fightOrSkip = function() {
 
 // fight function
 var fight = function(enemy) {
-  console.log(enemy);
-  //repeat and execute as long as the enemy-robot is alive
+
+  var isPlayerTurn = true;
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
+
   while (playerInfo.health > 0 && enemy.health > 0) {
+    if (isPlayerTurn) {
     if (fightOrSkip()) {
       break;
     }
@@ -58,7 +63,7 @@ var fight = function(enemy) {
     } else {
       window.alert(enemy.name + " still has " + enemy.health + " health left.");
     }
-
+  } else {
     // remove players's health by subtracting the amount set in the enemy.attack variable
     var damage = randomNumber(enemy.attack - 3, enemy.attack);
     
@@ -75,6 +80,8 @@ var fight = function(enemy) {
       window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
     }
   }
+  isPlayerTurn = !isPlayerTurn;
+ }
 };
 
   
